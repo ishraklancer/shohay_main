83d90426159868fd3c8f9b193deccba80de3a108
FarihaRaisa <raisafariha96@yahoo.com>
2018-10-03 01:33:24 +0600

    Easter Egg

--- app/src/main/java/com/shohay/shohay_main/About.java
+++ app/src/main/java/com/shohay/shohay_main/About.java
@@ -1,13 +1,26 @@
 package com.shohay.shohay_main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class About extends AppCompatActivity {
+
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        logo = findViewById(R.id.aboutLogo);
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(About.this, EasterEgg.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/shohay/shohay_main/HomeFragment.java
+++ app/src/main/java/com/shohay/shohay_main/HomeFragment.java
@@ -3,12 +3,19 @@ package com.shohay.shohay_main;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -16,8 +23,15 @@ import android.view.Window;
  */
 public class HomeFragment extends Fragment {
 
+    FragmentTransaction homeTransaction;
+    TextInputLayout moylaTab, napitTab, dhupiTab;
+
+
+
     public HomeFragment() {
         // Required empty public constructor
+
+
     }
 
 
@@ -29,6 +43,11 @@ public class HomeFragment extends Fragment {
         ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+
+
     }
+
+
+
 
 }
