4fdae39d852690852e47eb51086c55266b66ec92
fahad hasan <fahadhasan462@gmail.com>
2018-10-02 21:12:03 +0600

    more adapters, map and more errors :P

---
+++ app/src/main/java/com/shohay/shohay_main/SharedData.java
@@ -0,0 +1,10 @@
+package com.shohay.shohay_main;
+
+public class SharedData {
+
+    public static User user;
+    public static User user1;
+    public static User user2;
+    public static User user3;
+
+}
